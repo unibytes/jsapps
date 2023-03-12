344e26b80fdbe3848accde576e20050d430633dc
boxianglin <boxiang.lin@wsu.edu>
2023-03-11 17:51:43 -0800

    fix dynamic form so that a delete for a possible answer get shift up by one, letters always A,B,C ...

--- DynamicForm.js
+++ DynamicForm.js
@@ -6,53 +6,73 @@ class DynamicForm {
     addAnswerBtnClickBind(addButtonId) {
         const addAnsBtn = document.getElementById(addButtonId)
         addAnsBtn.addEventListener("click", function(){
-            
-            // calculate the next character 
-            const formLen = this.form.querySelectorAll("div").length
-            const nxtLetter = String.fromCharCode(65 + formLen)
-            const divContent = "Answer " + nxtLetter
-            
-            // add div
-            const newDiv = document.createElement("div")
-            newDiv.id = nxtLetter
-            newDiv.className = "form-group"
-            newDiv.textContent = divContent
 
-            // rmbtn inside the div
+            var possibleAnswerDiv = document.getElementById("possibleAnswers")
+            var numPossibleAnswer = possibleAnswerDiv.querySelectorAll("div") == null ? 0 : possibleAnswerDiv.querySelectorAll("div").length
+            var nxtLetter = String.fromCharCode(65 + numPossibleAnswer)
+    
+            // creat a new possible answer div
+            var newAnswerDiv = document.createElement("div")
+            var divContent = "Answer " + nxtLetter
+            newAnswerDiv.id = nxtLetter
+            newAnswerDiv.className = "form-group"
+            newAnswerDiv.textContent = divContent
+
+            // rmbtn a rmtbtn inside the new possible answer div 
             var rmbtn = document.createElement("button")
             rmbtn.textContent = "x"
             rmbtn.type = "button"
             rmbtn.className = "btn btn-danger btn-sm"
             rmbtn.addEventListener("click", function() {
-                this.form.removeChild(newDiv)
+                // shift to up by one
+                const answerDiv = possibleAnswerDiv.children
+                var startIdx = Array.prototype.indexOf.call(answerDiv, newAnswerDiv);
+                
+                for (let i = startIdx; i < answerDiv.length - 1; i++) {
+                    answerDiv[i].querySelector("textarea").value = answerDiv[i+1].querySelector("textarea").value
+                }
+                
+                // always delete the last possible ans 
+                possibleAnswerDiv.lastElementChild.remove()
+
+                // for selecting field, just delete the last
+                var ansSelect = document.getElementById("ansSelect")
+                var lastOption = ansSelect.options[ansSelect.options.length - 1];
+                lastOption.remove();
             })
-            newDiv.appendChild(rmbtn)
+            newAnswerDiv.appendChild(rmbtn)
 
-            // add newAnswer to div
-            var newAnswer = document.createElement("textarea")
-            newAnswer.className = "form-control"
-            newAnswer.id = "answer" + nxtLetter
-            newAnswer.placeholder = "Enter your answer"
-            newAnswer.style = "margin-top: 5px;"
-            newDiv.appendChild(newAnswer)
+            // create textarea field inside the new possinle answer div
+            var ansTextArea = document.createElement("textarea")
+            ansTextArea.className = "form-control"
+            ansTextArea.id = "answer" + nxtLetter
+            ansTextArea.placeholder = "Enter your answer"
+            ansTextArea.style = "margin-top: 5px;"
+            newAnswerDiv.appendChild(ansTextArea)
             
-            // add div to form
-            const secondToLast = this.form.children[formLen - 1];
-            this.form.insertBefore(newDiv, secondToLast)
+   
+            // put new possible answer div into possibleAnswerDiv
+            possibleAnswerDiv.appendChild(newAnswerDiv)
 
-            // add new option 
             const ansSelect = document.getElementById("ansSelect")
             const newOption = document.createElement("option")
             newOption.value = nxtLetter
             newOption.text = nxtLetter
             ansSelect.appendChild(newOption)
-
         }.bind(this));
     }
-
+ 
     publishButtonBind(pubButtonId) {
         const pubBtn = document.getElementById(pubButtonId)
+            // validate form
         pubBtn.addEventListener("click", function(){
+            // Validation Form -------------------------
+            if (document.getElementById("questionDiv").querySelector("textarea").value.length == 0) {
+                alert("Question Should Not Be Empty")
+                return
+            }
+
+            // Local Storage --------------------------
             let correctAnsLetter = "A"
             let question = ""
             let answers = {}
@@ -74,10 +94,14 @@ class DynamicForm {
                 // reset
                 txtarea.value = ""
             }
-            const quizItem = new QuizItem(localStorage.length, question, answers, correctAnsLetter)
-            localStorage.removeItem("myObj")
-            console.log(localStorage)
+            const qid = localStorage.length + 1
+            const quizItem = new QuizItem(qid, question, answers, correctAnsLetter)
+            // put quizItem into local storage
+            localStorage.setItem(qid, JSON.stringify(quizItem))
+            console.log(JSON.stringify(quizItem))
         }.bind(this))
 
     } 
+
+ 
 }
